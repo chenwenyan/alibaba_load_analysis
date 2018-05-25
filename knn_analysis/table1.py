60b1e7795a63139554423e8ebe6446d10593e0eb
chenwenyan <1358384755@qq.com>
2018-05-25 14:03:52 +0800

    add some sum attributes

--- knn_analysis/table1.py
+++ knn_analysis/table1.py
@@ -61,9 +61,10 @@ def statistics_data():
     instance = instance.iloc[:, 0].values
     print('total instance:' + str(len(instance)))
 
-    failed_tasks_arr, terminated_task_arr, waiting_task_arr = [], [], []
+    failed_tasks_arr, terminated_task_arr, waiting_task_arr, cancelling_task_arr = [], [], [], []
     instance_of_job_arr, instance_num_of_task_arr = [], []
     task_of_job_arr = []
+    instance_sum = 0
     for i in job:
         task_of_job = task_df[task_df.iloc[:, 2] == i]
         task_num_of_job = len(task_of_job.iloc[:, 0].values)
@@ -71,13 +72,17 @@ def statistics_data():
         failed_task_of_job = task_of_job[task_of_job.iloc[:, 5] == 'Failed']
         terminated_task_of_job = task_of_job[task_of_job.iloc[:, 5] == 'Terminated']
         waiting_task_of_job = task_of_job[task_of_job.iloc[:, 5] == 'Waiting']
+        cancelling_task_of_job = task_of_job[task_of_job.iloc[:, 5] == 'Cancelled']
         failed_tasks_arr.append(len(failed_task_of_job))
         terminated_task_arr.append(len(terminated_task_of_job))
         waiting_task_arr.append(len(waiting_task_of_job))
+        cancelling_task_arr.append(len(cancelling_task_of_job))
+        instance_sum = sum(task_df.iloc[:, 4].values)
 
     failed_task_sum = sum(failed_tasks_arr)
     terminated_task_num = sum(terminated_task_arr)
     waiting_task_sum = sum(waiting_task_arr)
+    cancelling_task_sum = sum(cancelling_task_arr)
     instance_of_job_sum = sum(instance_of_job_arr)
     avg_task_num_of_job = sum(task_of_job_arr) / len(job)
     max_task_num_of_job = max(task_of_job_arr)
@@ -86,10 +91,12 @@ def statistics_data():
     print('failed_task_sum:' + str(failed_task_sum))
     print('terminated_task_num:' + str(terminated_task_num))
     print('waiting_task_sum:' + str(waiting_task_sum))
+    print('cancelling_task_sum:' + str(cancelling_task_sum))
     print('instance_of_job_sum:' + str(instance_of_job_sum))
     print('avg_task_num_of_job:' + str(avg_task_num_of_job))
     print('max_task_num_of_job:' + str(max_task_num_of_job))
     print('min_task_num_of_job:' + str(min_task_num_of_job))
+    print('instance_sum:' + str(instance_sum))
 
     instance_arr = []
     for j in task:
@@ -105,7 +112,8 @@ def statistics_data():
 
     avg_instance_duration, max_instance_duration, min_instance_duration = 0, 0, 0
     avg_task_duration, max_task_duration, min_task_duration = 0, 0, 0
-    print(instance_df.groupby(instance_df.iloc[:,3]))
+    print(instance_df.groupby([instance_df.iloc[:, 2], instance_df.iloc[:, 3]]))
+    print(instance_df.groupby(instance_df.iloc[:, 3]).mean())
     # for indexs in instance_df.index:
     #     record = instance_df.loc[indexs].values[0:-1]
 
